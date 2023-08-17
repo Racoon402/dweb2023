00c4072e5c75d82d9de49ab21fd96fcbdf1c0c54
been <88560918+Racoon402@users.noreply.github.com>
2023-08-18 08:28:09 +0900

    Update map.js

--- map.js
+++ map.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
     accordionItems.forEach((item, index) => {
         const accordionButton = item.querySelector(".accordion-button");
         accordionButton.addEventListener("click", function () {
-            panTo(index);
+            panTo(index+1);
             console.log("클릭한 아이템의 인덱스:", index);
         });
     });
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function panTo(info) {
     // 이동할 위도 경도 위치를 생성합니다 
-    var moveLatLon = positions[info+1].latlng;
+    var moveLatLon = positions[info].latlng;
 
     // 지도 중심을 부드럽게 이동시킵니다
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
